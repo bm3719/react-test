e06960925a210242f2b55ced0d41907106190f15
Bruce C. Miller <bm3719@gmail.com>
2019-03-03 00:08:33 -0500

    Get basic react site working

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  entry: "./src/scripts/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: "src/index.html",
+      filename: "./index.html"
+    })
+  ],
   module: {
     rules: [
       {
